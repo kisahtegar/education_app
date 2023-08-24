431645e564dd5c48e3b2e3b39d686f61a7283f63
Kisah Tegar Putra Abdi <code.kisahtegar@gmail.com>
2023-08-24 22:43:58 +0700

    Implement OnBoarding UI

---
+++ lib/core/extensions/context_extension.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// Extension for context
+extension ContextExt on BuildContext {
+  ThemeData get theme => Theme.of(this);
+
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+
+  Size get size => mediaQuery.size;
+  double get width => size.width;
+  double get height => size.height;
+}
--- lib/src/on_boarding/presentation/views/on_boarding_screen.dart
+++ lib/src/on_boarding/presentation/views/on_boarding_screen.dart
@@ -1,20 +1,88 @@
+import 'package:education_app/core/common/views/loading_view.dart';
+import 'package:education_app/core/common/widgets/gradient_background.dart';
+import 'package:education_app/core/res/colours.dart';
+import 'package:education_app/core/res/media_res.dart';
+import 'package:education_app/src/on_boarding/domain/entities/page_content.dart';
+import 'package:education_app/src/on_boarding/presentation/widgets/on_boarding_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../cubit/on_boarding_cubit.dart';
 
-class OnBoardingScreen extends StatelessWidget {
+class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
 
   static const routeName = '/';
 
   @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  final pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<OnBoardingCubit>().checkIfUserIsFirstTimer();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BlocListener<OnBoardingCubit, OnBoardingState>(
-      listener: (context, state) {
-        // TODO: implement listener
-      },
-      child: Container(),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: GradientBackground(
+        image: MediaRes.onBoardingBackground,
+        child: BlocConsumer<OnBoardingCubit, OnBoardingState>(
+          listener: (context, state) {
+            if (state is OnBoardingStatus && !state.isFirstTimer) {
+              Navigator.pushReplacementNamed(context, '/home');
+            } else if (state is UserCached) {
+              // TODO(User-Cached-Handler): Push to the appropriate screen
+            }
+          },
+          builder: (context, state) {
+            if (state is CheckingIfUserIsFirstTimer ||
+                state is CachingFirstTimer) {
+              return const LoadingView();
+            }
+            return Stack(
+              children: [
+                PageView(
+                  controller: pageController,
+                  children: const [
+                    OnBoardingBody(pageContent: PageContent.first()),
+                    OnBoardingBody(pageContent: PageContent.second()),
+                    OnBoardingBody(pageContent: PageContent.third()),
+                  ],
+                ),
+                Align(
+                  alignment: const Alignment(0, .04),
+                  child: SmoothPageIndicator(
+                    controller: pageController,
+                    count: 3,
+                    onDotClicked: (index) {
+                      pageController.animateToPage(
+                        index,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeInOut,
+                      );
+                    },
+                    effect: const WormEffect(
+                      dotHeight: 10,
+                      dotWidth: 10,
+                      spacing: 40,
+                      activeDotColor: Colours.primaryColour,
+                      dotColor: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
